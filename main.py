e6400b05b9af225fe735a6ce97fad94a92e72e20
Vlad222222222 <100528611+Vlad222222222@users.noreply.github.com>
2022-03-06 22:41:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+for i in range(1, 11):
+    a = i * 1
+    b = i * 2
+    print(f"#{i} * {1} = {a}#     # {i} * {2} = {b}#") 
+
+for i in range(1, 11):
+    s = i * 3
+    d = i * 4
+    print(f"#{i} * {3} = {s}#     # {i} * {4} = {d}#") 
+
+for i in range(1, 11):
+    f = i * 5
+    u = i * 6
+    print(f"#{i} * {5} = {f}#     # {i} * {6} = {u}#") 
+
+for i in range(1, 11):
+    g = i * 7
+    j = i * 8
+    print(f"#{i} * {7} = {g}#     # {i} * {8} = {j}#") 
+
+for i in range(1, 11):
+    n = i * 9
+    m = i * 10
+    print(f"#{i} * {9} = {n}#     # {i} * {10} = {m}#") 
+
